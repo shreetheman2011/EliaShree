12db136e465576b1f7f4b0f9fcea65c8dbc71d1b
shreetheman2011 <shree.manickaraja@gmail.com>
2026-06-18 09:39:13 +0530

    made sure that it checks if the pose is null

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -29,25 +29,37 @@ public class Vision extends SubsystemBase {
     public VisionMeasurement getVisionMeasurement() {
         PoseEstimate limelight1Pose = LimelightHelpers.getBotPoseEstimate_wpiBlue(limelight1);
         PoseEstimate limelight2Pose = LimelightHelpers.getBotPoseEstimate_wpiBlue(limelight2);
-
         Pose2d averageOfPoses;
+
+        //for testing:
+        // limelight2Pose = new PoseEstimate();
+        // limelight2Pose.tagCount = 2;
+        // limelight2Pose.avgTagDist = 4;
+        // limelight1Pose = new PoseEstimate();
+        // limelight1Pose.tagCount = 2;
+        // limelight1Pose.avgTagDist = 4;
+
+        if(limelight1Pose != null && limelight2Pose != null){
+            averageOfPoses = new Pose2d(  (limelight1Pose.pose.getX() + limelight2Pose.pose.getX()) / 2,   (limelight1Pose.pose.getY() + limelight2Pose.pose.getY()) / 2,   (new Rotation2d((limelight1Pose.pose.getRotation().getRadians() + limelight2Pose.pose.getRotation().getRadians()) /2))  );
+        } else {
+            averageOfPoses= Pose2d.kZero;
+        }
  
-        averageOfPoses = new Pose2d(  (limelight1Pose.pose.getX() + limelight2Pose.pose.getX()) / 2,   (limelight1Pose.pose.getY() + limelight2Pose.pose.getY()) / 2,   (new Rotation2d((limelight1Pose.pose.getRotation().getRadians() + limelight2Pose.pose.getRotation().getRadians()) /2))  );
 
 
 
         //check if each cam has 2 tags at least in sight
         //if so, avg
-        if (limelight1Pose.tagCount >= 2 && limelight2Pose.tagCount >= 2 && limelight1Pose.avgTagDist < 5 && limelight2Pose.avgTagDist < 5 ){
+        if (limelight1Pose != null && limelight2Pose != null && limelight1Pose.tagCount >= 2 && limelight2Pose.tagCount >= 2 && limelight1Pose.avgTagDist < 5 && limelight2Pose.avgTagDist < 5 ){
             return new VisionMeasurement(averageOfPoses, ((limelight1Pose.timestampSeconds + limelight2Pose.timestampSeconds) / 2)); 
 
-        } else if (limelight2Pose.tagCount >=2 && limelight2Pose.avgTagDist < 5 ){
+        } else if (limelight2Pose != null && limelight2Pose.tagCount >=2 && limelight2Pose.avgTagDist < 5 ){
             //check if limelight2 sees more than 2 tags but limelight 1 doesnt
             
             return new VisionMeasurement(limelight2Pose.pose, limelight2Pose.timestampSeconds); 
 
         
-        } else if (limelight1Pose.tagCount >=2 && limelight1Pose.avgTagDist < 5 ) {
+        } else if (limelight1Pose != null && limelight1Pose.tagCount >=2 && limelight1Pose.avgTagDist < 5 ) {
             //check if limelight 1 sees more than 2 tags but limelight 2 doesnt
             return new VisionMeasurement(limelight1Pose.pose, limelight1Pose.timestampSeconds); 
 
